f41b0f7e05d009f466d9c1bebbf080a7f6252a28
Elena <elena.vilimaite@gmail.com>
2016-06-09 11:53:08 +0100

    day-four

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 feature "attacking players" do
-  scenario "player 1 is attacking player 2" do
-    sign_in_and_play
-    click_link 'ATTACK!!!'
-    expect(page).to have_content "name1 attacks name2"
-  end
+	scenario "player 1 is attacking player 2" do
+		sign_in_and_play
+		click_link 'ATTACK!!!'
+		expect(page).to have_content "name1 attacks name2"
+	end
+end
+
+feature "reducing player's points" do
+	scenario 'player 1 reduces player\'s 2 points by 10 ' do 
+		sign_in_and_play
+		click_link  'ATTACK!!!'
+		# expect(page).to have_content "name2 lost 10 points"
+		# expect(player_2_hit_points).to 
+	end
 end
